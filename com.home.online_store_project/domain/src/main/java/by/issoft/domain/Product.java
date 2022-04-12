05049c93a2b60514e3bba2a2a07dc35b6319ad73
Daria Dibal <dariadibal@gmal.com>
2022-04-12 03:27:49 +0300

    implement task 3

---
+++ com.home.online_store_project/domain/src/main/java/by/issoft/domain/Product.java
@@ -0,0 +1,18 @@
+package by.issoft.domain;
+
+public class Product {
+    private String name;
+    private Double price;
+    private Double rate;
+
+    public Product(String name, Double price, Double rate) {
+        this.name = name;
+        this.price = price;
+        this.rate = rate;
+    }
+    @Override
+    public String toString() {
+            String productInfo = String.format("Name:'%s', Price: %s,Rate: %s", name, price, rate);
+        return productInfo;
+    }
+}
