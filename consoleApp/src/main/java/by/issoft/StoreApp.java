175cbbe8733e991107ad9bcc4a98eeb83d562d93
Daria Dibal <dariadibal@gmal.com>
2022-11-19 20:40:51 +0300

    04 Interaction with Store

--- consoleApp/src/main/java/by/issoft/StoreApp.java
+++ consoleApp/src/main/java/by/issoft/StoreApp.java
@@ -2,11 +2,37 @@ package by.issoft;
 
 import by.issoft.helper.StoreHelper;
 
+import java.util.Scanner;
+
 public class StoreApp {
     public static void main(String[] args) {
         Store onlineStore = new Store();
         StoreHelper storeHelper = new StoreHelper(onlineStore);
         storeHelper.fillStoreRandomly();
         onlineStore.printAllCategoriesAndProducts();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Please use the following commands to interact with the store: sort, top, quit.");
+
+            switch (scanner.nextLine()) {
+                case "sort":
+                    System.out.println("Sorted list:");
+                    // todo sort
+                    break;
+                case "top":
+                    System.out.println("Top 5: ");
+                    // todo print top 5
+                    break;
+                case "quit":
+                    scanner.close();
+                    System.out.println("Bye");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Wrong command. Try again.");
+                    break;
+            }
+        }
     }
 }
